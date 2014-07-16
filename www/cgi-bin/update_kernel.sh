b5c63db2acda57255c1cfb5af97bbe15c83d3237
kindmong <kindmong@gmail.com>
2014-07-16 18:09:27 +0900

    firmware update modified

--- www/cgi-bin/update_kernel.sh
+++ www/cgi-bin/update_kernel.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-/bin/fwmng -i /tmp/new_fw -o /dev/mtdblock2
-echo "done"
+/bin/fwmng -i /tmp/new_fw -o /dev/mtdblock2 -v -V -U >& /tmp/fwmng.txt
+result=`cat /tmp/fwmng.txt | awk 'END { print }'` 
+if [ "$result" = "Firmware upgrade successfully finished." ]
+then
+	rm /tmp/fwmng.txt
+	echo "done"
+else
+	rm /tmp/fwmng.txt
+	echo "fail"
+	/www/cgi-bin/update_ramdisk.sh
+fi
