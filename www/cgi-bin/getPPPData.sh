20594bd14fd70e320e6cbbae67b316917a55955a
kindmong <kindmong@gmail.com>
2014-09-15 15:46:57 +0900

    rx tx 계산을 tmp 경로로 바꿈

--- www/cgi-bin/getPPPData.sh
+++ www/cgi-bin/getPPPData.sh
@@ -22,8 +22,8 @@ sum()
 {
 	initRx=`cat /etc/ppp/data/initData | awk '{ print $1 }'`
 	initTx=`cat /etc/ppp/data/initData | awk '{ print $2 }'`
-	currentRx=`cat /etc/ppp/data/curData | awk '{ print $1 }'`
-	currentTx=`cat /etc/ppp/data/curData | awk '{ print $2 }'`
+	currentRx=`cat /tmp/curData | awk '{ print $1 }'`
+	currentTx=`cat /tmp/curData | awk '{ print $2 }'`
 	sumRx=`expr $initRx + $currentRx`
 	sumTx=`expr $initTx + $currentTx`
 	`echo $sumRx $sumTx > /etc/ppp/data/initData`
@@ -32,13 +32,19 @@ sum()
 is_ppp_connected
 if [ $? -eq 0 ]
 then
-	initRx=`cat /etc/ppp/data/initData | awk '{ print $1 }'`
-	initTx=`cat /etc/ppp/data/initData | awk '{ print $2 }'`
-	currentRx=`cat /etc/ppp/data/curData | awk '{ print $1 }'`
-	currentTx=`cat /etc/ppp/data/curData | awk '{ print $2 }'`
-	sumRx=`expr $initRx + $currentRx`
-	sumTx=`expr $initTx + $currentTx`
-	echo "$sumRx" "$sumTx"
+	if [ -f /tmp/curData ]
+	then
+		initRx=`cat /etc/ppp/data/initData | awk '{ print $1 }'`
+		initTx=`cat /etc/ppp/data/initData | awk '{ print $2 }'`
+		currentRx=`cat /tmp/curData | awk '{ print $1 }'`
+		currentTx=`cat /tmp/curData | awk '{ print $2 }'`
+		sumRx=`expr $initRx + $currentRx`
+		sumTx=`expr $initTx + $currentTx`
+		echo "$sumRx" "$sumTx"
+	else
+		initData=`cat /etc/ppp/data/initData | awk '{ print $1, $2 }'`
+		echo "$initData"
+	fi
 else
 	initData=`cat /etc/ppp/data/initData | awk '{ print $1, $2 }'`
 	echo "$initData"
--- www/cgi-bin/reboot.sh
+++ www/cgi-bin/reboot.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
-initRx=`cat /etc/ppp/data/initData | awk '{ print $1 }'`
-initTx=`cat /etc/ppp/data/initData | awk '{ print $2 }'`
-currentRx=`cat /etc/ppp/data/curData | awk '{ print $1 }'`
-currentTx=`cat /etc/ppp/data/curData | awk '{ print $2 }'`
-sumRx=`expr $initRx + $currentRx`
-sumTx=`expr $initTx + $currentTx`
-`echo $sumRx $sumTx > /etc/ppp/data/initData`
+if [ -f /tmp/curData ]
+then 
+	initRx=`cat /etc/ppp/data/initData | awk '{ print $1 }'`
+	initTx=`cat /etc/ppp/data/initData | awk '{ print $2 }'`
+	currentRx=`cat /tmp/curData | awk '{ print $1 }'`
+	currentTx=`cat /tmp/curData | awk '{ print $2 }'`
+	sumRx=`expr $initRx + $currentRx`
+	sumTx=`expr $initTx + $currentTx`
+	`echo $sumRx $sumTx > /etc/ppp/data/initData`
+fi
 
 `echo AT#SHDN > /dev/ttyS1; sleep 0.1`
 sleep 5 
